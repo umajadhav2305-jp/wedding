e76ec5fa4f004161f94b64aaddb39ee62f59b1e0
unknown <umajadhav2305@-email@gmail.com>
2026-04-13 13:33:11 +0530

    my commit

--- ecommerce/src/app/about/page.js
+++ ecommerce/src/app/about/page.js
@@ -17,7 +17,6 @@ const page = () => {
         <div className="container">
           <div className="pages_name_wrap">
             <h1>ABOUT</h1>
-            <h2>hello react </h2>
           </div>
         </div>
       </div>
